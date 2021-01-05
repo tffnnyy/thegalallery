006962e803573cf57d4a6077a894e6114160db33
tffnnyy <twtyau@gmail.com>
2021-01-04 22:41:26 -0800

    Add files via upload

---
+++ galallery/main.js
@@ -0,0 +1,11 @@
+
+
+function openNav() {
+ document.getElementById("myNav").style.width = "100%";
+ console.log('it works!')
+}
+
+function closeNav() {
+ document.getElementById("myNav").style.width = "0%";
+   console.log('it works!')
+}
